c6420ad120b34ca27f0aa538779a60c409e6a8f7
Kenji Shirane <bkenro@gmail.com>
2024-07-12 08:09:24 +0900

    docker-ioからdocker-ceに移行, #9

--- build/initial.sh
+++ build/initial.sh
@@ -117,11 +117,25 @@ systemctl enable mailpit.service
 systemctl start mailpit.service
 sed -i -e "s|;sendmail_path =|sendmail_path = /usr/local/bin/mailpit sendmail|" /etc/php/8.3/apache2/php.ini
 
+# Remove docker-io
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
+# Install docker-ce
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Allow vagrant user to execute docker commands
+usermod -aG docker vagrant
 
 # Misc
 apt-get install zip -y
-apt-get install docker.io -y
-apt-get install docker-compose -y
 apt-get install imagemagick -y
 sed -i -e 's#<policy domain="coder" rights="none" pattern="PDF" />#<policy domain="coder" rights="read | write" pattern="PDF" />#' /etc/ImageMagick-6/policy.xml
 
