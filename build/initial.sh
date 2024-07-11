5c17ebeff09d1d9ffa1bf0214fd8859b64f15009
Kenji Shirane <bkenro@gmail.com>
2024-07-12 08:00:11 +0900

    MariaDB更新に際してrootパスワード設定方法を変更, #11

--- build/initial.sh
+++ build/initial.sh
@@ -12,7 +12,7 @@ timedatectl set-timezone Asia/Tokyo
 # MariaDB, security enhancemnet, adding interactive admin with user/pass 'dba'
 apt-get install mariadb-server -y
 mysql <<EOS
-UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
+ALTER USER root@localhost IDENTIFIED BY 'root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
